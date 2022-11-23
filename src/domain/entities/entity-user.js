05c23ddc2580cf98c56025dd80ed415ba67bce60
bearom95 <bearom95@gmail.com>
2022-11-23 19:21:49 +0100

    entities y repository

---
+++ src/domain/entities/entity-user.js
@@ -0,0 +1,26 @@
+//TODO OK, REVISADO
+
+module.exports = (db) => {
+  const userSchema = new db.Schema(
+    {
+      name: { type: String, required: true },
+      nickname: { type: String, required: true, unique: true },
+      email: { type: String, required: true, unique: true },
+      password: { type: String },
+      avatar: { type: String },
+      role: { type: String, default: normal },
+      comments: [{ type: db.Schema.Types.ObjectId, ref: 'Comments' }],
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+  //CONSULTAR !!!! y password required?????
+  /*  password?( userSchema.pre("save", function (next) {
+    this.password = bcrypt.hashSync(this.password, 10)
+    next()
+  })): (next()) */
+
+  return db.model('Users', userSchema);
+};
