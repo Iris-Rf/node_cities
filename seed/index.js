c1a91c9bdc522feadc2f533533790c74eec9b861
bearom95 <bearom95@gmail.com>
2022-11-26 20:02:37 +0100

    feat: semilla scrapeada y subida a mongo

--- seed/index.js
+++ seed/index.js
@@ -1,36 +1,62 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const http = require("http");
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+const City = require("./src/city/citySchema");
+const cors = require("cors");
+const express = require("express");
+const mongoURI = process.env.MONGO_URI;
 
-const cityInfoJSON = {};
-
-const browserURL = async (city) => {
+const browserURL = async () => {
   const browser = await puppeteer.launch();
 
   const page = await browser.newPage();
-  await page.goto(`https://es.wikipedia.org/wiki/${city}`);
+  await page.goto(`https://es.wikipedia.org/wiki/Tarragona`);
 
-  await page.screenshot({ path: `./screenshot/wiki-${city}.jpg` });
-  const cityInfo = await page.$$eval(".infobox.geography.vcard", (nodes) => {
-    return nodes.map((node) => {
-      name: node.querySelector(".cabecera.mapa.fn.org")?.innerText;
-    });
+  const cityInfo = await page.$$eval("#bodyContent", (nodes) => {
+    return nodes.map((node) => ({
+      name: node.querySelector(".cabecera.mapa.fn.org")?.innerText,
+      country: node.querySelector("[title='España']")?.innerText,
+      population: node.querySelector("tr:nth-child(17) > td")?.innerText,
+      mapImage: node.querySelector(
+        ".mw-kartographer-map.mw-kartographer-container.center.mw-kartographer-link > img"
+      )?.src,
+      history: node.querySelector(".rellink.noprint.hatnote ~ p")?.innerText,
+      places: "El puente del Diablo ",
+    }));
   });
-  cityInfoJSON = await cityInfo;
+  return cityInfo;
 };
 
-browserURL("Tarragona");
+const connect = async () => {
+  try {
+    const dbConnect = await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-cityInfoJSON = await JSON.stringify(cityInfo);
-fs.writeFile(
-  "./data.json",
-  cityInfoJSON,
-  (error) => error && console.log("Ha habido un error.")
-);
+    const cityInfo = await browserURL();
 
-const PORT = 8080;
-const server = http.createServer(requestHandler);
+    const allCities = await City.find();
+    if (allCities.length) await City.collection.drop();
 
-server.listen(PORT, () => {
-  console.log(`server started in http://localhost:${PORT}`);
-});
+    const citySeed = await cityInfo.map((city) => new City(city));
+    await City.insertMany(citySeed);
+
+    const { name, host } = dbConnect.connection;
+    console.log(`Conectado a la DB 👀: ${name} en el host❤️: ${host}`);
+  } catch (error) {
+    console.error(`No se ha podido conectar a la DB 💔`, error);
+  }
+};
+
+browserURL();
+
+connect();
+
+const server = express();
+
+server.use(cors());
+
+server.listen(process.env.PORT, console.log("Puerto levantado"));
